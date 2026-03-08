1852b7b8f119ea4f81e8e3645db2653a5c2ce5cd
Nethunter <kiktorrr@gmail.com>
2026-03-08 20:04:34 +0500

    build: embed MetrolistExtractor source to bypass JitPack outages

--- innertube/build.gradle.kts
+++ innertube/build.gradle.kts
@@ -7,6 +7,12 @@ kotlin {
     jvmToolchain(21)
 }
 
+sourceSets {
+    main {
+        java.srcDirs("external/java-extractor", "external/java-timeago")
+    }
+}
+
 dependencies {
     implementation(libs.ktor.client.core)
     implementation(libs.ktor.client.okhttp)
@@ -14,6 +20,20 @@ dependencies {
     implementation(libs.ktor.serialization.json)
     implementation(libs.ktor.client.encoding)
     implementation(libs.brotli)
-    implementation(libs.extractor)
+    
+    // Extractor dependencies (local source)
+    implementation(libs.nanojson)
+    implementation(libs.jsoup)
+    implementation(libs.java.websocket)
+    implementation(libs.spotbugs.annotations)
+    implementation(libs.autolink)
+    implementation(libs.protobuf.java)
+    implementation(libs.apache.lang3)
+    implementation(libs.json)
+    implementation(libs.cache2k.api)
+    implementation(libs.cache2k.core)
+    implementation(libs.rhino)
+    implementation("commons-codec:commons-codec:1.16.0")
+
     testImplementation(libs.junit)
 }
